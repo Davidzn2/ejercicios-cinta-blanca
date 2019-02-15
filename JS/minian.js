b65f68404548fa5f83aa179782bf67cf93e0b56e
davidzn2 <davidzn2@gmail.com>
2019-02-15 11:26:18 -0600

    Clase JS (Jueves)

---
+++ JS/minian.js
@@ -0,0 +1,11 @@
+// OR == ||
+// AND == &&
+// DIFERENTE QUE !=
+// IGUAL QUE ==
+var edad = 12
+var genero = 0
+if(edad >= 13 && genero == 0){
+    console.log("Cuentas para el minian")
+}else if(edad == 12 && genero == 0){
+    console.log("En un año cuentas para minian")
+}
