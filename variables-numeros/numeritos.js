f8e504936a5a8835b7bb957a392428dd69fca537
davidzn2 <davidzn2@gmail.com>
2019-02-18 22:53:04 -0600

    Clase 18 feb

---
+++ variables-numeros/numeritos.js
@@ -0,0 +1,9 @@
+var x = 13
+var y = 6
+
+var suma = x + y
+var resta = x - y 
+var multiplicacion = x * y
+var division = x / y
+var modulo = x % y
+var exponente = x ** 2
---
+++ variables-string/strings.js
@@ -0,0 +1,10 @@
+var nombre = "David"
+var numero = 5
+console.log(numero + nombre)
+
+var persona = prompt("Por favor mete tu nombre", "Harry Potter");
+
+alert("Hola " + persona + "! ¿Como estas?" + " " + persona)
+var str = "Hello World!";
+var res = str.toUpperCase();
+alert(res)
