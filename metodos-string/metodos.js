4feecaa33a475920bc76909e5ab9155283da61b4
davidzn2 <davidzn2@gmail.com>
2019-02-19 19:45:26 -0600

    Ejercicios con arreglos

---
+++ metodos-string/metodos.js
@@ -0,0 +1,8 @@
+let string1 = "holaa amigos!!!"
+let tamanio = string1.length
+let mayus = string1.charAt(0).toUpperCase()
+let minus = string1.split(1, tamanio).toLowerCase()
+let total = mayus + minus
+console.log(mayus)
+console.log(minus)
+console.log(total)
